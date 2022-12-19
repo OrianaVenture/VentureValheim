61c8f5959476a864fdef2f66b838344a7ed3d590
OrianaVenture <107574865+OrianaVenture@users.noreply.github.com>
2022-12-18 20:54:23 -0600

    BeyondTheEdge Version 0.1.0: Adding to github for tracking.

---
+++ BeyondTheEdge/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace VentureValheim.BeyondTheEdge
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class BeyondTheEdgePlugin : BaseUnityPlugin
+    {
+        private const string ModName = "BeyondTheEdge";
+        private const string ModVersion = "0.1.0";
+        private const string Author = "com.orianaventure.mod";
+        private const string ModGUID = Author + "." + ModName;
+
+        private readonly Harmony HarmonyInstance = new(ModGUID);
+
+        public static readonly ManualLogSource BeyondTheEdgeLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        public void Awake()
+        {
+            BeyondTheEdgeLogger.LogInfo("Disabling Edge Of World Kill.");
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            HarmonyInstance.PatchAll(assembly);
+        }
+
+        [HarmonyPatch(typeof(Player), nameof(Player.EdgeOfWorldKill))]
+        public static class Patch_Player_EdgeOfWorldKill
+        {
+            private static bool Prefix()
+            {
+                return false; // Skip this method
+            }
+        }
+    }
+}
---
+++ BeyondTheEdge/README.md
@@ -0,0 +1,22 @@
+# Beyond The Edge
+
+Created by [OrianaVentureMod@gmail.com](https://github.com/OrianaVenture/VentureValheim).
+
+## Introduction
+
+Stops you from dying when going off the edge of the world.
+
+## Changelog
+
+### 0.1.0
+
+* First release
+
+## Contributing
+
+All issues can be reported on the project Github. To report issues please be as specific as possible and provide the following:
+
+1. Version of this mod you are using.
+2. List of the other mods being used.
+
+All feedback, ideas, and requests are welcome! You can message me at my discord [Venture Gaming](https://discord.gg/tAd5hapt88).
