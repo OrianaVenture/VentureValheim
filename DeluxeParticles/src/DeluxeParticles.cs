c6d86a62999f2f906fc6d6f2b99a86b66efb0cfa
OrianaVenture <107574865+OrianaVenture@users.noreply.github.com>
2023-02-23 14:03:11 -0600

    Introducing Deluxe Particles 0.1.0: Enlarges the particle effect on dropped items so you can see them better.

---
+++ DeluxeParticles/src/DeluxeParticles.cs
@@ -0,0 +1,51 @@
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace VentureValheim.DeluxeParticles
+{
+    public class DeluxeParticles
+    {
+        private DeluxeParticles() {}
+        private static readonly DeluxeParticles _instance = new DeluxeParticles();
+
+        public static DeluxeParticles Instance
+        {
+            get => _instance;
+        }
+
+        [HarmonyPatch(typeof(ObjectDB), nameof(ObjectDB.Awake))]
+        public static class Patch_ObjectDB_Awake
+        {
+            private static void Postfix()
+            {
+                if (SceneManager.GetActiveScene().name.Equals("main"))
+                {
+                    Deluxify();
+                    DeluxeParticlesPlugin.DeluxeParticlesLogger.LogInfo("Done deluxifying particles.");
+                }
+            }
+        }
+
+        private static void Deluxify()
+        {
+            var items = ObjectDB.m_instance.m_items;
+
+            for (int lcv = 0; lcv < items.Count; lcv++)
+            {
+                var item = items[lcv].GetComponent<ParticleSystem>();
+                if (item != null)
+                {
+                    var itemMain = item.main;
+
+                    itemMain.startLifetime = new ParticleSystem.MinMaxCurve(3f);
+                    itemMain.startSize = new ParticleSystem.MinMaxCurve(0.6f, 0.8f);
+                    itemMain.maxParticles = 3;
+
+                    var emission = item.emission;
+                    emission.rateOverTime = new ParticleSystem.MinMaxCurve(1f);
+                }
+            }
+        }
+    }
+}
