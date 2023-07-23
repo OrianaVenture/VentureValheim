8d141a9ee9f998f149da5fb6b7c18674998acb2f
OrianaVenture <107574865+OrianaVenture@users.noreply.github.com>
2023-07-23 17:53:34 -0500

    Introducing Sea Of Noodles version 0.1.0: Increase spawn rates of serpents.

---
+++ SeaOfNoodles/Plugin.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine.SceneManagement;
+
+namespace VentureValheim.SeaOfNoodles
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class SeaOfNoodlesPlugin : BaseUnityPlugin
+    {
+        private const string ModName = "SeaOfNoodles";
+        private const string ModVersion = "0.1.0";
+        private const string Author = "com.orianaventure.mod";
+        private const string ModGUID = Author + "." + ModName;
+
+        private readonly Harmony HarmonyInstance = new(ModGUID);
+
+        public static readonly ManualLogSource SeaOfNoodlesLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static bool setup = false;
+
+        public void Awake()
+        {
+            SeaOfNoodlesLogger.LogInfo("I shall call him squishy and he shall be mine and he shall be my squishy!");
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            HarmonyInstance.PatchAll(assembly);
+        }
+
+        /// <summary>
+        /// Find the Serpent spawn entries and increase spawn rates
+        /// </summary>
+        [HarmonyPatch(typeof(SpawnSystem), nameof(SpawnSystem.Awake))]
+        public static class Patch_SpawnSystem_Awake
+        {
+            private static void Postfix(SpawnSystem __instance)
+            {
+                if (setup)
+                {
+                    return;
+                }
+
+                foreach (var spawn in __instance.m_spawnLists)
+                {
+                    foreach (var entry in spawn.m_spawners)
+                    {
+                        if (entry.m_prefab != null && entry.m_prefab.name == "Serpent")
+                        {
+                            SeaOfNoodlesLogger.LogDebug($"Rolling a Noodle to perfection.");
+                            entry.m_spawnInterval = 500f; // original 1000
+                            entry.m_spawnChance = 25f; // original 5
+                            entry.m_spawnDistance = 25f; // original 50
+                            entry.m_spawnAtDay = true;
+                        }
+                    }
+                }
+
+                setup = true;
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), nameof(Player.Awake))]
+        public static class Patch_Player_Awake
+        {
+            private static void Postfix()
+            {
+                if (!SceneManager.GetActiveScene().name.Equals("main"))
+                {
+                    // A starting menu is open, reset init in case of multi-server/game session
+                    setup = false;
+                }
+            }
+        }
+    }
+}
---
+++ SeaOfNoodles/README.md
@@ -0,0 +1,35 @@
+# Sea Of Noodles
+
+Created by [OrianaVentureMod@gmail.com](https://github.com/OrianaVenture/VentureValheim).
+
+## Introduction
+
+Increases the spawn rate of Serpents (aka Noodles).
+
+## Features
+
+* Decreases the spawn interval to 500 seconds (from 1000)
+* Increases the spawn chance to 25% (from 5)
+* Decreases the spawn distance to 25 (from 50)
+* Allows spawning during the day
+
+Please note that any the default rates in vanilla may change at any time and may be incorrect in this readme.
+
+## Installation
+
+This mod is client side only and has no configuration options. Does not need to be on the server.
+
+## Changelog
+
+### 0.1.0
+
+* First release
+
+## Contributing
+
+All issues can be reported on the project Github. To report issues please be as specific as possible and provide the following:
+
+1. Version of this mod you are using.
+2. List of the other mods being used.
+
+All feedback, ideas, and requests are welcome! You can message me at my discord [Venture Gaming](https://discord.gg/tAd5hapt88).
