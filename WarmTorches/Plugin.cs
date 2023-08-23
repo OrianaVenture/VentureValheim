7cb13c7612bf5f25868b9a06250d9b47322ae8a6
OrianaVenture <107574865+OrianaVenture@users.noreply.github.com>
2023-08-23 16:38:42 -0500

    Introducing Warm Torches 0.1.0!

---
+++ WarmTorches/Plugin.cs
@@ -0,0 +1,54 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace VentureValheim.WarmTorches
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class WarmTorchesPlugin : BaseUnityPlugin
+    {
+        private const string ModName = "WarmTorches";
+        private const string ModVersion = "0.1.0";
+        private const string Author = "com.orianaventure.mod";
+        private const string ModGUID = Author + "." + ModName;
+
+        private readonly Harmony HarmonyInstance = new(ModGUID);
+
+        public static readonly ManualLogSource WarmTorchesLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        public void Awake()
+        {
+            WarmTorchesLogger.LogInfo("Feeling Hot Hot Hot!");
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            HarmonyInstance.PatchAll(assembly);
+        }
+
+        /// <summary>
+        /// Only called when the Player checks environment. Check for torch here.
+        /// </summary>
+        [HarmonyPatch(typeof(EnvMan), nameof(EnvMan.IsCold))]
+        public static class Patch_EnvMan_IsCold
+        {
+            private static void Postfix(ref bool __result)
+            {
+                if (__result)
+                {
+                    var player = Player.m_localPlayer;
+                    if (player != null)
+                    {
+                        var rightItem = player.m_rightItem?.m_shared?.m_itemType ?? null;
+                        var leftItem = player.m_leftItem?.m_shared?.m_itemType ?? null;
+
+                        if ((rightItem != null && rightItem == ItemDrop.ItemData.ItemType.Torch) ||
+                            (leftItem != null && leftItem == ItemDrop.ItemData.ItemType.Torch))
+                        {
+                            __result = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WarmTorches/README.md
@@ -0,0 +1,32 @@
+# Warm Torches
+
+Created by [OrianaVentureMod@gmail.com](https://github.com/OrianaVenture/VentureValheim).
+
+## Introduction
+
+Torch items will prevent you from getting the Cold debuff.
+
+## Features
+
+Wielding a torch (or lantern) in either hand will take away the Cold debuff caused by environmental factors unless in a freezing biome. For example, a torch warms you up at night in meadows but does not keep you warm in the mountains.
+
+Should be compatible with custom items that are using the Torch ItemType.
+
+## Installation
+
+This mod is client side only and has no configuration options. Does not need to be on the server.
+
+## Changelog
+
+### 0.1.0
+
+* First release
+
+## Contributing
+
+All issues can be reported on the project Github. To report issues please be as specific as possible and provide the following:
+
+1. Version of this mod you are using.
+2. List of the other mods being used.
+
+All feedback, ideas, and requests are welcome! You can message me at my discord [Venture Gaming](https://discord.gg/tAd5hapt88).
