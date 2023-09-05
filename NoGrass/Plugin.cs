ad74a734ec3caa0f73dd1a08be2654d456e1dbe1
OrianaVenture <107574865+OrianaVenture@users.noreply.github.com>
2023-09-05 16:17:54 -0500

    Introducing No Grass version 0.1.0: adds back option to remove grass.

---
+++ NoGrass/Plugin.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using BepInEx;
+using HarmonyLib;
+
+namespace VentureValheim.NoGrass
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class NoGrassPlugin : BaseUnityPlugin
+    {
+        private const string ModName = "NoGrass";
+        private const string ModVersion = "0.1.0";
+        private const string Author = "com.orianaventure.mod";
+        private const string ModGUID = Author + "." + ModName;
+
+        private readonly Harmony HarmonyInstance = new(ModGUID);
+
+        public void Awake()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            HarmonyInstance.PatchAll(assembly);
+        }
+
+        /// <summary>
+        /// Increase slider options to include 0.
+        /// </summary>
+        [HarmonyPatch(typeof(Settings), nameof(Settings.Awake))]
+        public static class Patch_Settings_Awake
+        {
+            private static void Prefix(Settings __instance)
+            {
+                __instance.m_vegetation.minValue = 0f;
+            }
+        }
+
+        /// <summary>
+        /// Remove the subtraction from the vegetation setting to fix localization.
+        /// If there are any other instances of subtraction added within this method this may fail.
+        /// </summary>
+        [HarmonyPatch(typeof(Settings), nameof(Settings.Update))]
+        public static class Patch_Settings_Update
+        {
+            private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                for (var lcv = 1; lcv < codes.Count; lcv++)
+                {
+                    if (codes[lcv].opcode == OpCodes.Sub)
+                    {
+                        codes[lcv - 1].opcode = OpCodes.Nop; //ldc.i4.1
+                        codes[lcv].opcode = OpCodes.Nop; //sub
+                        break;
+                    }
+                }
+
+                return codes.AsEnumerable();
+            }
+        }
+    }
+}
---
+++ NoGrass/README.md
@@ -0,0 +1,30 @@
+# No Grass
+
+Created by [OrianaVentureMod@gmail.com](https://github.com/OrianaVenture/VentureValheim).
+
+## Introduction
+
+Adds back the option to select no grass in your game settings vegetation slider.
+
+## Features
+
+This mod increases the vegetation quality slider entries by one (to a total of 4) to include an option for the "no grass" setting removed in the Hildir update.
+
+## Installation
+
+This mod is client side and has no configuration options.
+
+## Changelog
+
+### 0.1.0
+
+* First release
+
+## Contributing
+
+All issues can be reported on the project Github. To report issues please be as specific as possible and provide the following:
+
+1. Version of this mod you are using.
+2. List of the other mods being used.
+
+All feedback, ideas, and requests are welcome! You can message me at my discord [Venture Gaming](https://discord.gg/tAd5hapt88).
