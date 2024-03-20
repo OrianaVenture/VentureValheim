cd2f591897c279b975d2ea7e1bdaf5ab3faa9ea7
Nicolas Busseneau <nicolas@busseneau.fr>
2024-03-20 06:08:31 +0100

    progression tests: add `KeyManager.HasGuardianKey(...)` tests
    In 842611cdb8896c599241f494db1436ad2c32ae2d, we added support for
    multiple guardian powers in `KeyManager.HasGuardianKey(...)` checks.
    
    Since this is a bit more complex that the previous system, add tests
    ensuring it behaves as intended.

--- ProgressionTests2/src/KeyTests.cs
+++ ProgressionTests2/src/KeyTests.cs
@@ -1,4 +1,4 @@
-using Moq;
+﻿using Moq;
 using Xunit;
 using VentureValheim.Progression;
 
@@ -30,6 +30,7 @@ namespace VentureValheim.ProgressionTests
             public void UpdateGlobalKeyConfigurationTest(string a, string b) => UpdateGlobalKeyConfiguration(a, b);
             public void UpdatePrivateKeyConfigurationTest(string a, string b) => UpdatePrivateKeyConfiguration(a, b);
             public int CountPrivateBossKeysTest() => CountPrivateBossKeys();
+            public bool HasGuardianKeyTest(string guardianPower) => HasGuardianKey(guardianPower);
             public bool PrivateKeyIsBlockedTest(string key) => PrivateKeyIsBlocked(key);
             protected override bool HasGlobalKey(string key) => hasGlobalKeyReturnValue;
             public bool SummoningTimeReachedTest(string key, int gameDay) => SummoningTimeReached(key, gameDay);
@@ -186,6 +187,28 @@ namespace VentureValheim.ProgressionTests
             var keyManager = new TestKeyManager(mockManager.Object);
 
             Assert.Equal(expected, keyManager.CountPrivateBossKeysTest());
+        }
+
+        [Theory]
+        [InlineData("GP_TheElder", "", false)]
+        [InlineData("GP_TheElder", "defeated_eikthyr", false)]
+        [InlineData("GP_TheElder", "defeated_eikthyr,defeated_gdking", true)]
+        [InlineData("GP_Eikthyr,GP_Bonemass", "defeated_eikthyr,defeated_gdking", false)]
+        [InlineData("GP_Eikthyr,GP_Bonemass", "defeated_eikthyr,defeated_gdking,defeated_bonemass", true)]
+        [InlineData("GP_Eikthyr,GP_TheElder,GP_Bonemass", "defeated_eikthyr,defeated_gdking", false)]
+        [InlineData("GP_Eikthyr,GP_TheElder,GP_Bonemass", "defeated_eikthyr,defeated_gdking,defeated_bonemass", true)]
+        public void HasGuardianKeyTest(string guardianPower, string keys, bool expected)
+        {
+            var mockKeyManager = new Mock<IKeyManager>();
+            var set = ProgressionAPI.StringToSet(keys);
+            mockKeyManager.SetupGet(x => x.PrivateKeysList).Returns(set);
+            var keyManager = new TestKeyManager(mockKeyManager.Object, hasGlobalKeyReturnValue: false);
+
+            var mockProgressionConfiguration = new Mock<IProgressionConfiguration>();
+            mockProgressionConfiguration.Setup(x => x.GetUsePrivateKeys()).Returns(true);
+            new ProgressionConfiguration(mockProgressionConfiguration.Object);
+
+            Assert.Equal(expected, keyManager.HasGuardianKeyTest(guardianPower));
         }
 
         [Theory]
