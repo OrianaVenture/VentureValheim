dfca8ea170800a30e9ae4b5706e3980fc89a1c00
OrianaVenture <107574865+OrianaVenture@users.noreply.github.com>
2024-04-26 16:15:53 -0500

    No Damage Flash first release.

---
+++ NoDamageFlash/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+
+namespace VentureValheim.NoDamageFlash
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class NoDamageFlashPlugin : BaseUnityPlugin
+    {
+        private const string ModName = "NoDamageFlash";
+        private const string ModVersion = "0.1.0";
+        private const string Author = "com.orianaventure.mod";
+        private const string ModGUID = Author + "." + ModName;
+
+        private readonly Harmony HarmonyInstance = new(ModGUID);
+
+        public void Awake()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            HarmonyInstance.PatchAll(assembly);
+        }
+
+        /// <summary>
+        /// Disable damage flash by never applying it.
+        /// </summary>
+        [HarmonyPatch(typeof(Hud), nameof(Hud.DamageFlash))]
+        public static class Patch_Hud_DamageFlash
+        {
+            private static bool Prefix()
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ NoDamageFlash/README.md
@@ -0,0 +1,30 @@
+# No Damage Flash
+
+Created by [OrianaVentureMod@gmail.com](https://github.com/OrianaVenture/VentureValheim).
+
+## Introduction
+
+Removes the red flash that happens when you take damage.
+
+## Features
+
+Until it becomes a base game feature this mod will exist to remove the red hud flash when your character takes damage. Also recommended to turn on the vanilla "reduce flashing lights" accessibility setting which reduces lightning flashing effects.
+
+## Installation
+
+This mod is client side and has no configuration options.
+
+## Changelog
+
+### 0.1.0
+
+* First release
+
+## Contributing
+
+All issues can be reported on the project Github. To report issues please be as specific as possible and provide the following:
+
+1. Version of this mod you are using.
+2. List of the other mods being used.
+
+All feedback, ideas, and requests are welcome! You can message me at my discord [Venture Gaming](https://discord.gg/tAd5hapt88).
