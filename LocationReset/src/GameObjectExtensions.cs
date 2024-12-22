336f302e942baa59bca57675e5cd7211fce51da7
searica <143636061+searica@users.noreply.github.com>
2024-12-22 07:51:20 -0800

    Handle getting prefab names (works for MineRock5)
    Adds extension method to get prefab name that is aware of MineRock5 renaming and MineRock5PrefabTracker component.
    
    Adds extension method to add a MineRock5PrefabTracker and set m_prefabName before the object is ever spawned. This is to prevent the prefab name being set after MineRock5.Awake changes it.

---
+++ LocationReset/src/GameObjectExtensions.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace VentureValheim.LocationReset
+{
+    internal static class GameObjectExtensions
+    {
+        private const string CloneSuffix = "(Clone)";
+        private const string MineRock5Name = "___MineRock5";
+
+        /// <summary>
+        ///     Gets the root Prefab name by recursively stripping the suffix "(Clone)".
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// <returns></returns>
+        internal static string GetPrefabName(this GameObject gameObject)
+        {
+            string prefabName = gameObject.name;
+            if (prefabName.Contains(MineRock5Name))
+            {
+                if (gameObject.TryGetComponent(out MineRock5PrefabTracker tracker))
+                {
+                    return tracker.m_prefabName;
+                }
+            }
+            while (prefabName.EndsWith(CloneSuffix))
+            {
+                prefabName = prefabName.Substring(0, prefabName.Length - CloneSuffix.Length);
+            }
+            return prefabName;
+        }
+
+
+        /// <summary>
+        ///     Adds MineRock5Tracker component and sets `m_prefabName` to be the root prefab name of the GameObject.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        internal static void AddMineRock5PrefabTracker(this GameObject gameObject)
+        {
+            if (gameObject.GetComponent<MineRock5PrefabTracker>())
+            {
+                return;
+            }
+            var mineRock5PrefabTracker = gameObject.AddComponent<MineRock5PrefabTracker>();
+            mineRock5PrefabTracker.m_prefabName = gameObject.GetPrefabName();
+        }
+    }
+}
