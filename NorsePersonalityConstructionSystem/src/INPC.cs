9439cf1d8daa4d1d7db988b58ae88dbbe2b24d5c
OrianaVenture <107574865+OrianaVenture@users.noreply.github.com>
2025-10-10 14:46:05 -0500

    NPCS: delete comments in INPC.cs

--- NorsePersonalityConstructionSystem/src/INPC.cs
+++ NorsePersonalityConstructionSystem/src/INPC.cs
@@ -1,16 +1,6 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-namespace VentureValheim.NPCS;
+﻿namespace VentureValheim.NPCS;
 
 public interface INPC
 {
     public NPCData Data { get; }
-    /*public void SetFromConfig(NPCConfiguration.NPCConfig config, bool newSpawn);
-    public void SetRandom();
-    public void SetName(string name);
-    public void SetSpawnPoint(Vector3 position);
-    public void SetTrueDeath(bool death);
-    public void SetRotation(Quaternion rotation);
-    public void Attach(bool attach, Chair chair = null);*/
 }
