de24426a473da6e6166d5db6425144c8ec471106
OrianaVenture <107574865+OrianaVenture@users.noreply.github.com>
2026-02-17 13:05:07 -0600

    Finished mod No Vegvisir Hints Version 0.2.0

--- NoVegvisirHints/Plugin.cs
+++ NoVegvisirHints/Plugin.cs
@@ -1,7 +1,7 @@
-﻿using System.Reflection;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using System.Reflection;
 
 namespace VentureValheim.NoVegvisirHints;
 
@@ -10,7 +10,7 @@ namespace VentureValheim.NoVegvisirHints;
 public class NoVegvisirHintsPlugin : BaseUnityPlugin
 {
     private const string ModName = "NoVegvisirHints";
-    private const string ModVersion = "0.1.0";
+    private const string ModVersion = "0.2.0";
     private const string Author = "com.orianaventure.mod";
     private const string ModGUID = Author + "." + ModName;
 
@@ -25,17 +25,41 @@ public class NoVegvisirHintsPlugin : BaseUnityPlugin
     }
 }
 
-[HarmonyPatch(typeof(Game), nameof(Game.DiscoverClosestLocation))]
-public static class Patch_Game_DiscoverClosestLocation
+/// <summary>
+/// Set any Vegvisir location to an invalid pin to prevent map pin from being discovered.
+/// </summary>
+[HarmonyPatch(typeof(Vegvisir), nameof(Vegvisir.Interact))]
+public static class Patch_Vegvisir_Interact
 {
-    private static bool Prefix(int pinType)
+    private static void Prefix(Vegvisir __instance)
     {
-        if (pinType == (int)Minimap.PinType.Boss ||
-            pinType == (int)Minimap.PinType.Hildir1 ||
-            pinType == (int)Minimap.PinType.Hildir2 ||
-            pinType == (int)Minimap.PinType.Hildir3)
+        if (__instance.m_locations.Count > 1)
         {
-            return false; // Skip method
+            // Do not block Hildir map pins
+            return;
+        }
+
+        foreach (Vegvisir.VegvisrLocation location in __instance.m_locations)
+        {
+            location.m_pinType = (Minimap.PinType)(-1);
+        }
+    }
+}
+
+/// <summary>
+/// Intercept and skip method for any invalid pins.
+/// </summary>
+[HarmonyPatch(typeof(Minimap), nameof(Minimap.DiscoverLocation))]
+public static class Patch_Minimap_DiscoverLocation
+{
+    private static bool Prefix(Minimap.PinType type)
+    {
+        Minimap.PinType invalid = (Minimap.PinType)(-1);
+
+        if (type == invalid)
+        {
+            NoVegvisirHintsPlugin.NoVegvisirHintsLogger.LogInfo("Map pin blocked!");
+            return false;
         }
 
         return true;
--- NoVegvisirHints/README.md
+++ NoVegvisirHints/README.md
@@ -1,3 +1,26 @@
-# TODO
+# No Vegvisir Hints
 
-This is unpublished.
+Created by [OrianaVentureMod@gmail.com](https://github.com/OrianaVenture/VentureValheim).
+
+## Introduction
+
+Disables the map pins from Vegvisir runestones in the world. Interacting with boss location runestones and the Hildir map table still adds pins as expected.
+
+## Installation
+
+This mod is client side only and has no configuration options. Does not need to be on the server.
+
+## Changelog
+
+### 0.2.0
+
+* First official release.
+
+## Contributing
+
+All issues can be reported on the project Github. To report issues please be as specific as possible and provide the following:
+
+1. Version of this mod you are using.
+2. List of the other mods being used.
+
+All feedback, ideas, and requests are welcome! You can message me at my discord [Venture Gaming](https://discord.gg/tAd5hapt88).
